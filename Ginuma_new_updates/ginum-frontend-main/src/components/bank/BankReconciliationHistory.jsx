aa326b92bf7f0966124009a6cbc0255e2f9f0681
shehan <shehan8998@gmail.com>
2026-07-13 11:45:12 +0530

    add ui for bank reconcilation fix error in build

--- Ginuma_new_updates/ginum-frontend-main/src/components/bank/BankReconciliationHistory.jsx
+++ Ginuma_new_updates/ginum-frontend-main/src/components/bank/BankReconciliationHistory.jsx
@@ -60,6 +60,7 @@ function BankReconciliationHistory() {
   };
 
   return (
+    <>
     <div className="max-w-6xl mx-auto p-4 sm:p-6 my-4 w-full">
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6">
         <h2 className="text-2xl font-bold text-gray-900 flex items-center">
