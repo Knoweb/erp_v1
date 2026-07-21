2d79fc42c2507dcbbb5e328841f376079c77fbea
shehan <shehan8998@gmail.com>
2026-07-21 14:11:29 +0530

    fix the customers display errors fix 3

--- Ginuma_new_updates/ginum-backend-main/src/main/java/com/example/GinumApps/service/CustomerService.java
+++ Ginuma_new_updates/ginum-backend-main/src/main/java/com/example/GinumApps/service/CustomerService.java
@@ -24,6 +24,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
